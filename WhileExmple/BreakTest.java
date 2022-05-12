5284ea75916cdb8a208c04fdee1e19943f5a2359
SeolVin <83351001+SeolVin@users.noreply.github.com>
2022-05-12 10:15:05 +0900

    Add files via upload

---
+++ WhileExmple/BreakTest.java
@@ -0,0 +1,11 @@
+
+public class BreakTest {
+
+	public static void main(String[] args) {
+		while(true) {
+			int num = (int)(Math.random()*6)+1;
+			System.out.println(num);
+		}
+	}
+
+}
---
+++ WhileExmple/WhileExample.java
@@ -0,0 +1,80 @@
+
+public class WhileExample {
+
+	public static void main(String[] args) {
+		int i=1;
+		int sum=0;
+		while(i <=100) {
+			sum += i;
+			i++;
+			
+		
+		
+		System.out.println(i);
+		System.out.println(sum);
+		
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
